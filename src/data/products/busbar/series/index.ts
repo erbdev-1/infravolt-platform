62f6c0ea0ad3b26e2fe84bd8dc8ee4e846328026
erbdev-1 <erbdev@hotmail.com>
2026-08-02 03:26:24 +0100

    feat: add GR cast resin busbar product page
    Adds the GR-A/GR-C cast resin busbar detail page with technical
    specification tabs, components, applications and installation guide,
    and registers it in the busbar series lookup.

--- src/data/products/busbar/series/index.ts
+++ src/data/products/busbar/series/index.ts
@@ -5,6 +5,7 @@ import { GGD_SYSTEM_DETAIL } from "./ggd";
 import { GL_SYSTEM_DETAIL } from "./gl";
 import { GM_SYSTEM_DETAIL } from "./gm";
 import { GNL_SYSTEM_DETAIL } from "./gnl";
+import { GR_SYSTEM_DETAIL } from "./gr";
 import { GS_SYSTEM_DETAIL } from "./gs";
 import type { BusbarSystemDetail, BusbarSystemDetailByMarket } from "./types";
 
@@ -16,6 +17,7 @@ const BUSBAR_SERIES_DETAIL: Partial<
   "gm-low-power-busbar": GM_SYSTEM_DETAIL,
   "ggd-medium-power-busbar": GGD_SYSTEM_DETAIL,
   "gs-super-compact": GS_SYSTEM_DETAIL,
+  "gr-cast-resin": GR_SYSTEM_DETAIL,
 };
 
 export function getBusbarSystemDetail(
